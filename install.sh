23a6fb02400ad7a5adc1cae0bbd17e2d62ed7a5c
Ayu <79645007+Ace-c@users.noreply.github.com>
2025-06-25 16:38:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+echo "copyig files..."
 
 if [ -d ~/.config ]; then
     cp -r .config/* ~/.config/
@@ -6,11 +8,17 @@ else
     cp -r .config ~/
 fi
 
+tar -xf .icons/Icons.tar.zst -C .icons/
+rm .icons/Icons.tar.zst
+
 if [ -d ~/.icons ]; then
     cp -r .icons/* ~/.icons/
 else
     cp -r .icons ~/
 fi
+
+tar -xf .themes/Theme.tar.zst -C .themes/
+rm .themes/Theme.tar.zst
 
 if [ -d ~/.themes ]; then
     cp -r .themes/* ~/.themes/
@@ -21,13 +29,33 @@ fi
 if [ -d ~/.local/share/fonts ]; then
     cp -r fonts/* ~/.local/share/fonts/
 else
-    mkdir -p ~/.local/share/fonts
-    cp -r fonts/* ~/.local/share/fonts/
+    cp -r fonts ~/.local/share/
 fi
+# Update font cache
+fc-cache -fv
 
 if [ -f ~/.zshrc ]; then
     mv ~/.zshrc ~/.zshrc.bak
 fi
 cp .zshrc ~/
 cp .gtkrc-2.0 ~/
+
+# Check for oh-my-zsh & both pugins "zsh-autosuggenstions", "zsh-syntax-highlighting-git" are Installed or not
+if ! command -v yay &>/dev/null; then
+    echo "'yay' is not installed. Skipping oh-my-zsh plugin setup."
+    echo "Please install yay first: https://aur.archlinux.org/packages/yay"
+    exit 0
+fi
+
+if yay -Q oh-my-zsh-git &>/dev/null; then
+    if yay -Q zsh-autosuggestions &>/dev/null; then
+        sudo ln -s /usr/share/zsh/plugins/zsh-autosuggestions /usr/share/oh-my-zsh/custom/plugins/
+    fi
+
+    if yay -Q zsh-syntax-highlighting-git &>/dev/null; then
+        sudo ln -s /usr/share/zsh/plugins/zsh-syntax-highlighting /usr/share/oh-my-zsh/custom/plugins/
+    fi
+else
+    echo "'oh-my-zsh-git' is not installed. Skipping plugin symlink setup."
+fi
 
